f79fda44fd8038a6f03bb8f00716ad65cd155e68
WendyOmollo <omollow280@gmail.com>
2019-09-12 12:48:33 +0300

    Add a background image to the layout file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,15 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
-public class App {
+    public class App {
+        public static void main(String[] args) {
+            staticFileLocation("/public");
+            Map<String, Object> model = new HashMap<>();
 
-}
+            get("/",(request, response) -> {
+                return new ModelAndView(model,"layout.hbs");
+            },new HandlebarsTemplateEngine());
+
+
+        }
+    }
