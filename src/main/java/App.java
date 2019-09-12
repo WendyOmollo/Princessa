40908b7e0092c5cb66f672b348de4ec8837924c5
WendyOmollo <omollow280@gmail.com>
2019-09-12 12:13:14 +0300

    Add plugins and dependencies to the app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,15 @@
+
+import models.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+
 }
