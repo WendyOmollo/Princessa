365a968a61f64d05c3d4b0174e7c004e328ae9c5
WendyOmollo <omollow280@gmail.com>
2019-09-12 11:37:40 +0300

    Add a test that updates a princesses details

--- src/main/java/models/Princess.java
+++ src/main/java/models/Princess.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class Princess {
     private String name;
     private int id;
+    private int age;
     private String strength;
     private String weakness;
     private static ArrayList<Princess> princesses = new ArrayList<>();
 
-    public Princess(String name,int id,String strength,String weakness){
+    public Princess(String name,int id,int age,String strength,String weakness){
         this.name = name;
-        this.id = princesses.size();
+
+        this.age = age;
         this.strength=strength;
         this.weakness = weakness;
         princesses.add(this);
@@ -31,6 +33,14 @@ public class Princess {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getStrength() {
@@ -58,5 +68,11 @@ public class Princess {
     }
     public static Princess findById(int id){
         return princesses.get(id-1);
+    }
+    public void update(String name,int age,String strength,String weakness){
+        this.name = name;
+        this.age = age;
+        this.strength=strength;
+        this.weakness= weakness;
     }
 }
--- src/test/java/models/PrincessTest.java
+++ src/test/java/models/PrincessTest.java
@@ -16,7 +16,7 @@ public class PrincessTest {
     public void tearDown() throws Exception {
     }
     public Princess setUpNewPrincess(){
-        return new Princess("Rapunzel",1,"Hair","Tower");
+        return new Princess("Rapunzel",1,12,"Hair","Tower");
     }
     @Test
     public void addPrincess_true() throws Exception{
@@ -32,5 +32,23 @@ public class PrincessTest {
     public void addPrincess_findPrincessById() throws Exception{
         Princess princess = setUpNewPrincess();
         assertEquals(Princess.findById(princess.getId()),princess);
+    }
+    @Test
+    public void addPrincess_updatePrincess() throws Exception{
+        Princess princess = setUpNewPrincess();
+        String formerName = princess.getName();
+        int formerAge = princess.getAge();
+        int formerId = princess.getId();
+        String formerStrength = princess.getStrength();
+        String formerWeakness = princess.getWeakness();
+
+        princess.update("Bella",2,"Rose flowers","Love");
+
+        assertEquals(formerId,princess.getId());
+        assertNotEquals(formerAge,princess.getAge());
+        assertNotEquals(formerName,princess.getName());
+        assertNotEquals(formerStrength,princess.getStrength());
+        assertNotEquals(formerWeakness,princess.getWeakness());
+
     }
 }
